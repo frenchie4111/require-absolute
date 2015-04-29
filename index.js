eac5cf004116587442e2912e4d16ac2278f0529f
Michael Lyons <mdl0394@gmail.com>
2015-04-29 14:01:46 -0700

    Added documentation to index.js

--- index.js
+++ index.js
@@ -10,12 +10,11 @@
 
     var absolute = require( 'absolute' ),
         path = require( 'path' );
-
-    var requireAbsolute = function( absolute_path ) {
-        var relative = path.relative( __dirname, removeExtension( absolute_path ) );
-        require( './' + relative );
-    };
-
+    /**
+     * Requires the file with the given absolute path. Throws error if specified is not an absolute path
+     * @param absolute_path The path to require
+     * @returns {*} The required file
+     */
     module.exports = function( absolute_path ) {
         if( !absolute( absolute_path ) ) throw new Error( 'Requires absolute path' );
         var relative = path.resolve( './', path.relative( __dirname, absolute_path ) );
